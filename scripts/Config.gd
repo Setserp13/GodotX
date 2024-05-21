9e7a427601f661f3c32a2d520d8adc45ebb66540
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-05-21 18:52:51 -0300

    Add files via upload

---
+++ scripts/Config.gd
@@ -0,0 +1,22 @@
+class Config:
+
+static var file_path = 'user://config.cfg'
+var file
+static var _instance = null
+
+func _init():
+	file = ConfigFile.new()
+	if ResourceLoader.exists(file_path):
+		file.load(file_path)
+
+static func get_value(player, key, value):
+	return _instance.file.get_value(player, key)
+
+static func set_value(player, key, value):
+	_instance.file.set_value(player, key, value)
+	_instance.file.save(file_path)
+
+static func get_instance():
+	if _instance == null:
+		_instance = Config.new()
+	return _instance
