1799ce202800fa1c5fbdbe3a241cafec39abfd83
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-05-26 23:23:00 -0300

    Update TiledSprite.gd

--- scripts/2D/TiledSprite.gd
+++ scripts/2D/TiledSprite.gd
@@ -3,8 +3,10 @@ extends Sprite2D
 
 class_name TiledSprite
 
+@export var tiling_per_scale = Vector2.ONE
+
 func _process(_delta):
-	material.set_shader_parameter('tiling', global_scale)
+	material.set_shader_parameter('tiling', global_scale / tiling_per_scale)
 
 static func tile(sprite):
 	var dir = 'res://addons/GodotX/scripts/2D/'
