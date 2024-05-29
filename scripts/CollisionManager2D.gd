6a66babe6c0af2656a3ab36f786a4a3fbf91d8f2
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-05-29 00:52:11 -0300

    Update CollisionManager2D.gd

--- scripts/CollisionManager2D.gd
+++ scripts/CollisionManager2D.gd
@@ -6,9 +6,11 @@ class_name CollisionManager2D #manage all collisions and triggers of an object
 @export var trigger = true
 var _on_collision_enter = Event1.new()
 var _on_collision_exit = Event1.new()
+var _on_collision_stay = Event1.new()
 var _collisions = []
 var _on_trigger_enter = Event1.new()
 var _on_trigger_exit = Event1.new()
+var _on_trigger_stay = Event1.new()
 var _triggers = []
 var _body
 
@@ -30,16 +32,23 @@ func process_collisions():
 	for x in collisions.filter(func(x): return not _collisions.any(func(y): return x.get_collider() == y.get_collider())):
 		_on_collision_enter.invoke(x)
 		xObject.try_call(x.get_collider(), 'on_collision_enter', [_body])
+	for x in collisions:
+		_on_collision_stay.invoke(x)
+		xObject.try_call(x.get_collider(), 'on_collision_stay', [_body])
 	_collisions = collisions
 
 func process_triggers():
 	var triggers = x2D.get_intersecting_areas(xNode.get_component(_body, CollisionShape2D))
+	#print(triggers)
 	for x in _triggers.filter(func(x): return x not in triggers):
 		_on_trigger_exit.invoke(x)
 		xObject.try_call(x, 'on_trigger_exit', [_body])
 	for x in triggers.filter(func(x): return x not in _triggers):
 		_on_trigger_enter.invoke(x)
 		xObject.try_call(x, 'on_trigger_enter', [_body])
+	for x in triggers:
+		_on_trigger_stay.invoke(x)
+		xObject.try_call(x, 'on_trigger_stay', [_body])
 	_triggers = triggers
 
 
@@ -60,8 +69,14 @@ static func on_collision_enter(body, callback):
 static func on_collision_exit(body, callback):
 	get_or_add(body)._on_collision_exit.add(callback)
 
+static func on_collision_stay(body, callback):
+	get_or_add(body)._on_collision_stay.add(callback)
+
 static func on_trigger_enter(body, callback):
 	get_or_add(body)._on_trigger_enter.add(callback)
 
 static func on_trigger_exit(body, callback):
 	get_or_add(body)._on_trigger_exit.add(callback)
+
+static func on_trigger_stay(body, callback):
+	get_or_add(body)._on_trigger_stay.add(callback)
