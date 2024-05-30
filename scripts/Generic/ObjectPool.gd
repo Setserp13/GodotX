8fc81c4c64112b4ac4934978ff7ba7ace5621bad
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-05-29 23:12:57 -0300

    Update ObjectPool.gd

--- scripts/Generic/ObjectPool.gd
+++ scripts/Generic/ObjectPool.gd
@@ -1,15 +1,17 @@
 class_name ObjectPool #object pooling
 
 var prefab
+var parent
 var _pool = []
 
-func _init(prefab):
+func _init(prefab, parent=null):
 	self.prefab = prefab
+	self.parent = parent
 
 func pick():
 	if _pool.size() > 0:
 		return _pool.pop_at(0)
-	return prefab.duplicate(4)
+	return xNode.append_child(parent, prefab.duplicate(4))
 
 func release(item):
 	_pool.append(item)
