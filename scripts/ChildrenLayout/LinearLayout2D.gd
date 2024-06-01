a5787da31a7a5bf7f68a31f7d23b1f9966ea6c57
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-05-31 23:44:28 -0300

    Update LinearLayout2D.gd

--- scripts/ChildrenLayout/LinearLayout2D.gd
+++ scripts/ChildrenLayout/LinearLayout2D.gd
@@ -2,17 +2,9 @@ extends ChildrenLayout
 
 class_name LinearLayout2D
 
-@export var immediate = false
 @export var direction = Vector2.RIGHT #: Variant
 @export var offset = 0
 @export var cell_size = 1
 @export var spacing = 0
-@export var speed = 10
 	
-func update_child(delta, x, i):
-	var target = direction * (offset + (cell_size + spacing) * i)
-	if immediate:
-		x.position = target
-	else:
-		x.position = x.position.move_toward(target, speed)
-	return x.position == target
+func get_position(i): return direction * (offset + (cell_size + spacing) * i)
