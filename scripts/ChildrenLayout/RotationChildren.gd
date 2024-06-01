1f642cf8ab47e88d936423d9b0b36bb0ad8aa0cb
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-05-31 23:47:27 -0300

    Update RotationChildren.gd

--- scripts/ChildrenLayout/RotationChildren.gd
+++ scripts/ChildrenLayout/RotationChildren.gd
@@ -2,14 +2,6 @@ extends ChildrenLayout
 
 class_name ChildrenRotation
 
-@export var angular_speed = 180
 var _cell_size
 
-func update_child(delta, x, i):
-	var angle = _cell_size * i
-	var target = angle - PI / 2
-	if Engine.is_editor_hint():
-		x.rotation = target
-	else:
-		x.rotation = rotate_toward(x.rotation, target, deg_to_rad(angular_speed))
-	return x.rotation == target
+func get_rotation(i): return (_cell_size * i) - PI / 2
