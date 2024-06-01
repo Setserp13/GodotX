671724819264e1801d3f98d7443f890e84e5f920
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-05-31 23:46:25 -0300

    Update AngularLayout2D.gd

--- scripts/ChildrenLayout/AngularLayout2D.gd
+++ scripts/ChildrenLayout/AngularLayout2D.gd
@@ -5,7 +5,6 @@ class_name AngularLayout2D
 @export var min = 7 * PI / 12 #from 0 to 2PI
 @export var size = -PI / 6 #from -2PI to 2PI
 @export var translation_after_highlighted = -0.05 #from -2PI to 2PI
-@export var angular_speed = 180
 #@export var z_delta = -0.01
 
 func get_rotation(i): return angle - PI / 2
