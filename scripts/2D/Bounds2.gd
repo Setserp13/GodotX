2d31a8683112e23811d7e30df862e9261fe84b5c
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-06-02 11:35:31 -0300

    Add files via upload

---
+++ scripts/2D/Bounds2.gd
@@ -0,0 +1,16 @@
+@tool
+extends Node2D
+
+class_name Bounds2
+
+@export var rect = Rect2(0, 0, 100, 100):
+	get: return rect
+	set(value):
+		rect = value
+		queue_redraw()
+
+func _draw(): draw_rect(rect, Color(1, 0, 0, 0.5), false)
+
+func _notification(what):
+	if what == NOTIFICATION_DRAW:
+		_draw()
