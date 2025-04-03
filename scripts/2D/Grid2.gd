9d1568fd7be6cb89bf3fb25d9480c2bfe2d88a02
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-04-03 20:31:25 -0300

    Update Grid2.gd

--- scripts/2D/Grid2.gd
+++ scripts/2D/Grid2.gd
@@ -10,10 +10,10 @@ func _init(offset, cell_size, cell_gap=Vector2.ZERO):
 	self.cell_gap = cell_gap
 
 func cell_to_point(value):
-	return self.offset + (self.cellSize + self.cellGap) * value
+	return self.offset + (self.cell_size + self.cell_gap) * value
 
 func point_to_cell(value):
-	return ((value - self.offset) / (self.cellSize + self.cellGap)).floor()
+	return ((value - self.offset) / (self.cell_size + self.cell_gap)).floor()
 	
 func cell(i, j):
 	return Rect2(self.cell_to_point(Vector2(i,j)), self.cell_size)
